058a856572ba74d6b556e163c3943348f6d44e7d
raketaboy2000 <43524452+raketaboy2000@users.noreply.github.com>
2019-06-06 13:05:46 +0300

    Add files via upload

--- lesson-17/src/js/main.js
+++ lesson-17/src/js/main.js
@@ -1,9 +1,10 @@
 var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
-button.addEventListener('click',function(){
-  modal.classList.add('modal_acrtive');
-});
-close.addEventListener('click',function(){
-  modal.classList.remove('modal_acrtive');
-});
+
+button.addEventListener('click', function () {
+  modal.classList.add('modal_active');
+})
+close.addEventListener('click', function () {
+  modal.classList.remove('modal_active');
+})
